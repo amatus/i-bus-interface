c54053d8927bc3fa5cd2bf4ddf3623e449684f19
David Barksdale <amatus.amongus@gmail.com>
2013-05-15 21:57:56 -0500

    Created new IBusIntents android project.

---
+++ android/IBusIntents/src/name/amatus/ibusintents/IBusIntentsActivity.java
@@ -0,0 +1,15 @@
+package name.amatus.ibusintents;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IBusIntentsActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
